8c978dbd4847f7eccfcfc5779ceaee43b1c36b8c
gygy2006 <gygy2006@naver.com>
2020-02-03 21:23:15 +0900

    airbnb

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,20 @@
 from math import ceil
-from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, redirect
 from . import models
 
 
 # request, response objects ↓
 # https://docs.djangoproject.com/en/3.0/ref/request-response/
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", context={"rooms": rooms},)
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(page)
+        return render(request, "rooms/home.html", context={"page": rooms},)
+    except EmptyPage:
+        return redirect("/")
+    except PageNotAnInteger:
+        return redirect("/")
 

c259062041029b45d3b177fbd7e40aaa6c023715
gygy2006 <gygy2006@naver.com>
2020-03-11 23:03:56 +0900

    20200311

--- config/settings.py
+++ config/settings.py
@@ -53,10 +53,7 @@ PROJECT_APPS = [
 # django_countries : https://github.com/SmileyChris/django-countries
 # django_seed : https://pypi.org/project/django-seed/
 
-THIRD_PARTY_APPS = [
-    "django_countries",
-    "django_seed",
-]
+THIRD_PARTY_APPS = ["django_countries", "django_seed", "storages"]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
@@ -183,7 +180,17 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
 # Sentry
 
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECERET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "airbnb-clone-rlagudtjq"
+AWS_AUTO_CREATE_BUCKET = True
+AWS_BUCKET_ACL = "public-read"
 if not DEBUG:
+    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
+    # django-storages s3
+
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
         integrations=[DjangoIntegration()],
